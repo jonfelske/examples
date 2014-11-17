7271d33eee1bb7d44ec74dadaa3dcd84168c0a77
Jon Felske <jonathan.felske@gmail.com>
2014-11-16 21:07:17 -0500

    Integrated Facebook SDK and login

--- ios/login/login/ViewController.swift
+++ ios/login/login/ViewController.swift
@@ -19,6 +19,32 @@ class ViewController: UIViewController {
     @IBAction func login(sender: AnyObject) {
         
         spinnerView.startAnimating()
+        
+        // Set permissions required from the facebook user account
+       // NSArray *permissionsArray = @[ @"user_about_me", @"user_relationships", @"user_birthday", @"user_location"];
+        let permissions = ["user_about_me", "user_likes", "user_birthday", "user_location", "user_relationships"]
+        
+        PFFacebookUtils.logInWithPermissions(permissions, {
+            (user: PFUser!, error: NSError!) -> Void in
+            if user == nil {
+                NSLog("Uh oh. The user cancelled the Facebook login.")
+                self.stopSpinner()
+            } else if user.isNew {
+                NSLog("User signed up and logged in through Facebook!")
+                 self.stopSpinner()
+            } else {
+                NSLog("User logged in through Facebook!")
+                self.stopSpinner()
+                 self.performSegueWithIdentifier("loggedIn", sender: nil)
+            }
+        })
+        
+        
+    }
+    
+    func stopSpinner(){
+        
+        spinnerView.stopAnimating()
     }
     
 
